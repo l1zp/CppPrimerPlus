95c25206a50ba63d27837ce03786a25882bb5be0
lizp <lizp@LAPTOP-TAO1IC4C.localdomain>
2020-06-28 21:55:26 +0800

    init

---
+++ Chap.3/bondini.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "\aOperation \"HyperHype\" is now activated!\n";
+	cout << "Enter your agent code: ____\b\b\b\b";
+	long code;
+	cin >> code;
+	cout << "\aYou entered " << code << "...\n";
+	cout << "\aCode vertified! Proceed with Plan Z3!\n";
+	return 0;
+}
