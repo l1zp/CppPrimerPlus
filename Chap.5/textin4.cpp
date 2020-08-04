0c0eda552258d5a132e5cd6aeedd39c8694214fc
Zhipeng LI <lzpthu@163.com>
2020-08-04 21:45:29 +0800

    finish chapter 5

---
+++ Chap.5/textin4.cpp
@@ -0,0 +1,16 @@
+// 0516 reading chars with a while loop
+#include <iostream>
+int main()
+{
+	using namespace std;
+	char ch;
+	int count = 0;
+
+	while ((ch = cin.get()) != EOF)
+	{
+		cout.put(char(ch));
+		++count;
+	}
+	cout << endl << count << " characters read\n";
+	return 0;
+}
